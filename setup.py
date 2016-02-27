23754ec63e81f9abc27ef7fea52cf1aa9e4714ad
jeffrobots <jeffbaatz@gmail.com>
2016-02-27 18:15:02 -0500

    Fixed setup.py
    Forgot to commit the setup file after creating PyPi last night.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
 	version = '1.0',
 	description = 'Sensor and Controls Management for Automated Greenhouse based on I2C',
 	author = 'Jeff Baatz',
+	author_email = 'jeffbaatz@gmail.com',
 	url = 'https://github.com/ECESeniorDesign/greenhouse-sensors',
-	download_url = 'https://github.com/ECESeniorDesign/greenhouse_envmgmt/tarball/1.0'
+	download_url = 'https://github.com/ECESeniorDesign/greenhouse_envmgmt/tarball/1.0',
 	keywords = ['i2c', 'sensors', 'controls'],
-	classifies = [],
-	install_requires=['python-smbus']
+	classifiers = [],
 )
 
